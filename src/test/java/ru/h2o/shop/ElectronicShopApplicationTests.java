89a19c84ebd9ad137c8ab3f82067525031ea6442
axel <i@amusaev.ru>
2024-08-05 19:32:47 +0300

    init

---
+++ src/test/java/ru/h2o/shop/ElectronicShopApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.h2o.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectronicShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
