36d97f3b75f17ac97a28b21b8e22a202d243fffb
axel <i@amusaev.ru>
2024-08-11 14:44:50 +0300

    change update(), findById(), findAllByType()

--- src/main/java/ru/h2o/shop/service/ProductService.java
+++ src/main/java/ru/h2o/shop/service/ProductService.java
@@ -1,11 +1,13 @@
 package ru.h2o.shop.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import ru.h2o.shop.entity.*;
+import ru.h2o.shop.exception.InvalidProductTypeException;
+import ru.h2o.shop.exception.ProductNotFoundException;
 import ru.h2o.shop.repository.ProductRepository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class ProductService {
@@ -20,35 +22,41 @@ public class ProductService {
     }
 
     public Product findById(Long id) {
-        return productRepository.findById(id).orElseThrow();
+        return productRepository.findById(id)
+                .orElseThrow(() -> new ProductNotFoundException("Product not found with id: " + id));
     }
 
+    @Transactional
     public Product update(Long id, Product product) {
-        Optional<Product> productOptional = productRepository.findById(id);
-        if (productOptional.isPresent()) {
-            Product newProduct = productOptional.get();
-            newProduct.setSerialNumber(product.getSerialNumber());
-            newProduct.setManufacturer(product.getManufacturer());
-            newProduct.setPrice(product.getPrice());
-            newProduct.setStockQuantity(product.getStockQuantity());
+        return productRepository.findById(id)
+                .map(newProduct -> {
+                    newProduct.setSerialNumber(product.getSerialNumber());
+                    newProduct.setManufacturer(product.getManufacturer());
+                    newProduct.setPrice(product.getPrice());
+                    newProduct.setStockQuantity(product.getStockQuantity());
 
-            if (product instanceof Desktop) {
-                ((Desktop) newProduct).setFormFactor(((Desktop) product).getFormFactor());
-            } else if (product instanceof Laptop) {
-                ((Laptop) newProduct).setDiagonal(((Laptop) product).getDiagonal());
-            } else if (product instanceof Monitor) {
-                ((Monitor) newProduct).setScreenSize(((Monitor) product).getScreenSize());
-            } else if (product instanceof HardDrive) {
-                ((HardDrive) newProduct).setMemorySize(((HardDrive) product).getMemorySize());
-            }
+                    if (product instanceof Desktop) {
+                        ((Desktop) newProduct).setFormFactor(((Desktop) product).getFormFactor());
+                    } else if (product instanceof Laptop) {
+                        ((Laptop) newProduct).setDiagonal(((Laptop) product).getDiagonal());
+                    } else if (product instanceof Monitor) {
+                        ((Monitor) newProduct).setScreenSize(((Monitor) product).getScreenSize());
+                    } else if (product instanceof HardDrive) {
+                        ((HardDrive) newProduct).setMemorySize(((HardDrive) product).getMemorySize());
+                    } else {
+                        throw new InvalidProductTypeException(product.getClass().getSimpleName());
+                    }
 
-            return productRepository.save(newProduct);
-        }
-
-        return productRepository.save(product);
+                    return productRepository.save(newProduct);
+                }).orElseThrow(() -> new ProductNotFoundException("Product not found with id: " + id));
     }
 
     public List<Product> findAllByType(String type) {
-        return productRepository.findAllByType(type);
+        List<Product> products = productRepository.findAllByType(type);
+        if (products.isEmpty()) {
+            throw new ProductNotFoundException("No products found of type: " + type);
+        }
+
+        return products;
     }
 }
