272b6a19b6d44a40412bff22b01b2c640dc4db9c
axel <i@amusaev.ru>
2024-08-11 14:45:09 +0300

    add GlobalExceptionHandler

---
+++ src/main/java/ru/h2o/shop/exception/GlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package ru.h2o.shop.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ProductNotFoundException.class)
+    public ResponseEntity<String> handleProductNotFoundException(ProductNotFoundException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(InvalidProductTypeException.class)
+    public ResponseEntity<String> handleInvalidProductTypeException(InvalidProductTypeException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleGeneralException(Exception ex) {
+        return new ResponseEntity<>("Internal server error", HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
